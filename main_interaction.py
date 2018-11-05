4c28467e6768914d30f27df0738a6592c787dc3b
AmSoLit <ammar.litim@gmail.com>
2018-11-05 14:15:35 +0100

    Update main_interaction.py

--- main_interaction.py
+++ main_interaction.py
@@ -22,11 +22,11 @@ df = Data(path)
 
 #Enregistrement de l'appel de la fonction encoding qui renvoie l'encodage du document
 
-bool = input("Afficher l'encodage ?")
+bool = input("Afficher l'encodage ? (o/n) ")
 
 encoding = df.encoding()
 
-if (bool == "o") :
+if (bool.lower() == "o") :
 
     encoding = df.encoding()
     print(encoding + "\n")
@@ -38,11 +38,11 @@ else :
 
 #Enregistrement de l'appel de la fonction size_ko qui renvoie la taille du  fichier en kilo octects
 
-bool = input("Afficher la taille du fichier ?")
+bool = input("Afficher la taille du fichier ? (o/n) ")
 
 size = df.size_ko()
 
-if (bool == "o") :
+if (bool.lower() == "o") :
 
     
     print(size + "\n")
@@ -53,11 +53,11 @@ else :
 
 #Enregistrement de l'appel de la fonction date_last_modif qui renvoie la derniène date de modification du fichier
 
-bool = input("Afficher la dernière date de modification ?")
+bool = input("Afficher la dernière date de modification ? (o/n) ")
 
 last_modif = df.date_last_modif()
 
-if (bool == "o") :
+if (bool.lower() == "o") :
 
     
     print(last_modif + "\n")
@@ -69,10 +69,10 @@ else :
 
 #Affichage du nombre de variables et d'observations
 
-bool = input("Afficher le nombre de variables et d'observations ?")
+bool = input("Afficher le nombre de variables et d'observations ? (o/n) ")
 
 
-if (bool == "o") :
+if (bool.lower() == "o") :
 
     print("Ce fichier présente " +  str(df.nb_variables()) + " variables et comptabilise " + str(df.nb_observations()) + " observations")
     print("\n")
@@ -83,11 +83,11 @@ else :
 
 #Affichage des informations concernant la table: nombre de variables qualitatives, nombre de variables quantitaives et les noms de ces variables.
 
-bool = input("Afficher le nombre de variables qualitatives, le nombre de variables quantitaves et les noms de ces variables ?")
+bool = input("Afficher le nombre de variables qualitatives, le nombre de variables quantitaves et les noms de ces variables ? (o/n) ")
 
 type_vari=""
 
-if (bool == "o") :
+if (bool.lower() == "o") :
     
     type_vari=df.type_var()
     print("\n")
@@ -96,27 +96,14 @@ else:
     
     pass
 
-#Affichage du minimum, du maximum, de la moyenne, de la médiane et de l'écart type de chaque variable quantitative
-
-bool = input("Afficher les maximums, minimums, médianes, moyennes et écart-type ?")
-
-max_min_etc = ""
-
-if (bool == "o") :
-    
-    max_min_etc = df.max_min_etc()
-    print("\n")
-
-else :
-    
-    pass
-
-#Affichage des modalités de la variables qualitatives
-
-bool = input("Afficher les modalités de la variables qualitatives ?")
 
 
-if (bool == "o") :
+#Affichage des modalités de la variable qualitative
+
+bool = input("Afficher les modalités de la variables qualitatives ? (o/n) ")
+
+
+if (bool.lower() == "o") :
     
     print(df.nb_valeurs())
     print("\n")
@@ -126,26 +113,44 @@ else :
     pass
 
 #Affichage de la fréquence
-bool = input("Afficher les fréquences ?")
+bool = input("Afficher les fréquences ? (o/n) ")
 
+str1=df.freq_valeurs()
 
-if (bool == "o") :
+if (bool.lower() == "o") :
     
-    print(df.freq_valeurs())
+    print(str1)
     print("\n")
 
 else :
     
     pass
 
+
+#Affichage du minimum, du maximum, de la moyenne, de la médiane et de l'écart type de chaque variable quantitative
+
+bool = input("Afficher les maximums, minimums, médianes, moyennes et écart-type ? (o/n) ")
+
+max_min_etc = ""
+
+if (bool.lower() == "o") :
+    
+    max_min_etc = df.max_min_etc()
+    print("\n")
+
+else :
+    
+    pass
+
+
 #Retourne histogramme
 
-bool = input("Enregistrer un histogramme ?")
+bool = input("Enregistrer un histogramme ? (o/n) ")
 
-if (bool == "o") :
+if (bool.lower() == "o") :
     
     df.hist_figure_quantitative()
-    print("Histogramme enregistré \n")
+    print("Histogramme enregistré  à côté du script principal sous le nom de \"plot.pdf\" \n")
 
 else :
     
@@ -161,8 +166,9 @@ if ((wantlog == 'o') | (wantlog =='O')):
    log.ecriture(encoding+"\n")
    log.ecriture(size+"\n")
    log.ecriture("Ce fichier présente " +  str(df.nb_variables()) + " variables et comptabilise " + str(df.nb_observations()) + " observations \n")
-   #log.ecriture(str1)
    log.ecriture(str(type_vari)+"\n")
+   log.ecriture(df.nb_valeurs())
+   log.ecriture(str1+"\n")
    log.ecriture(str(max_min_etc))
     
 elif ((wantlog == 'n') | (wantlog == 'N')) :
